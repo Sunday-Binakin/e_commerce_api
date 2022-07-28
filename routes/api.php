ca779da41d9deeef7f854130f504e6f6ce4aa4ee
Sunday-Binakin <sundaybinakin@gmail.com>
2022-07-28 10:49:08 +0100

    created models resources

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Whoops\RunInterface;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('/products','ProductController');
+Route::group(['prefix'=>'products'], function(){
+    Route::apiResource('/{product}/reviews','ReviewController'); 
 });
